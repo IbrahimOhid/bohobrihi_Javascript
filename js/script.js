a5398ddf4617418e64ad75cf052ef4e1ae4982de
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2023-09-17 21:54:02 +0600

    submitEvent addec

--- js/script.js
+++ js/script.js
@@ -1206,3 +1206,56 @@ olItem.className = "React";
 //     //console.log(e.target.className);
 //     //console.log(e.target.value);
 // }
+
+// const input = document.querySelector("input[name=name]");
+// input.addEventListener("change", changeHandler);
+
+// function changeHandler(e){
+//     //console.log(e);
+//     //console.log(e.type);
+//     //console.log(e.target);
+//     //console.log(e.target.className);
+//     //console.log(e.target.id);
+//     console.log(e.target.value);
+
+// }
+
+// const programs = document.querySelectorAll("input[name=program]");
+// Array.from(programs).map((program) => {
+//     program.addEventListener("change", programHandeler);
+// })
+
+// function programHandeler(e){
+//     if(e.target.checked){
+//         console.log(e.target.value);
+//     }
+// }
+
+// var select = document.querySelector("#department");
+// select.addEventListener("change", selectHandeler);
+// function selectHandeler(e){
+//     console.log(e.target.value);
+// }
+
+const form = document.querySelector("form");
+const name = form.querySelector("div #name");
+const email = form.querySelector("div #email");
+const password = form.querySelector("div #password");
+
+form.addEventListener("submit", submitHandeler);
+
+function submitHandeler(e) {
+    e.preventDefault();
+    // console.log(name.value);
+    // console.log(email.value);
+    // console.log(password.value);
+    const userInfo = {
+        name: name.value,
+        email: email.value,
+        password: password.value,
+    }
+    console.log(userInfo);
+    name.value = "";
+    email.value = "";
+    password.value = "";
+}
