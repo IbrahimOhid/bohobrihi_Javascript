ae9c1743a17029bf3b078f6d41d73c1923d4b7b1
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2023-10-12 22:25:28 +0600

    spread operator added

--- js/es6.js
+++ js/es6.js
@@ -26,7 +26,32 @@
 // name();
 // name('Hello Mohammad');
 
-function sum(x, y, ...z){
-    console.log(`x = ${x}, y = ${y}, z = ${z}`);
+// function sum(x, y, ...z){
+//     console.log(`x = ${x}, y = ${y}, z = ${z}`);
+// }
+// sum(10, 20, 30, 40, 50, 60);
+
+// function addNumbers(x, y, z){
+//     return x + y + z;
+// }
+// let num = [1,2,3];
+// //console.log(addNumbers(...num));
+// let num1 = [4, ...num, 5];
+// console.log(num1);
+
+// let num1 = [1, 2, 3];
+// let num2 = [4, 5, 6];
+// let numbers = [...num1, ...num2];
+// console.log(numbers);
+
+let p1 = {
+    Name: 'ibrahim',
+    age: 26
 }
-sum(10, 20, 30, 40, 50, 60);
+let p2 = {
+    nationality: 'Bangladesh',
+    Union: 'Islamabad'
+}
+
+let p = {...p1, ...p2};
+console.log(p);
