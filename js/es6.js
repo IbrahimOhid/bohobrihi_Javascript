8f135f21be58c9b60cc102fa0b734175a9e13739
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2023-10-21 21:51:24 +0600

    synchronous vs asynchronous added

--- js/es6.js
+++ js/es6.js
@@ -403,3 +403,33 @@
 // console.log(student2);
 // console.log(student2.id);
 // console.log(student2.name);
+
+const taskOne = () => {
+    console.log('Task1');
+}
+
+const dataLoading = () => {
+    console.log('Task2 Data Loading');
+}
+
+const taskTwo = () => {
+    setTimeout(dataLoading, 1000);
+}
+
+const taskThree = () => {
+    console.log('Task3');
+}
+
+const taskFour = () => {
+    console.log('Task4');
+}
+
+const taskFive = () => {
+    console.log('Task5');
+}
+
+taskOne();
+taskTwo();
+taskThree();
+taskFour();
+taskFive();
