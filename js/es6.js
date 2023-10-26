d25304dce505abde41d3968d4f3f5b2496bd5bed
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2023-10-26 22:00:35 +0600

    async & await added

--- js/es6.js
+++ js/es6.js
@@ -508,14 +508,33 @@ const taskFour = () => new Promise((resolve, reject) => {
     resolve('Task 4 Completed');
 })
 
-taskOne()
-.then((res) => console.log(res))
-.then(taskTwo)
-.then((res) => console.log(res))
-.then(taskThree)
-.then((res) => console.log(res))
-.then(taskFour)
-.then((res) => console.log(res))
-.catch((err) => console.log(err))
+// taskOne()
+// .then((res) => console.log(res))
+// .then(taskTwo)
+// .then((res) => console.log(res))
+// .then(taskThree)
+// .then((res) => console.log(res))
+// .then(taskFour)
+// .then((res) => console.log(res))
+// .catch((err) => console.log(err))
+
+//async & await
+
+const callAllTask = async () => {
+    try {
+        const t1 = await taskOne();
+        console.log(t1);
+        const t2 = await taskTwo();
+        console.log(t2);
+        const t3 = await taskThree();
+        console.log(t3);
+        const t4 = await taskFour();
+        console.log(t4);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+callAllTask();
 
 console.log('Bye');
